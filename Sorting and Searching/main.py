78712a8ae431d6e7301b8f9864bca7452913af9d
luisespriella9 <lfd2tb@virginia.edu>
2019-10-05 12:42:48 -0400

    Problem 10.3 Search in Rotated Array Solved

--- Sorting and Searching/main.py
+++ Sorting and Searching/main.py
@@ -40,6 +40,62 @@ def groupAnagrams(list):
         output.append(anagramGroup)
     return output
 
+#problem 10.2
+def searchSorted(array, target):
+    return searchSortedIter(array, target, 0, len(array)-1)
+
+def searchSortedIter(array, target, leftIndex, rightIndex):
+    if (rightIndex-leftIndex < 0): 
+        #if length == 0
+        return None
+    midIndex = int((leftIndex+rightIndex)/2)
+    midVal = array[midIndex]
+    if (midVal == target):
+        return midIndex
+    lastVal = array[rightIndex]
+    if (lastVal == target):
+        return rightIndex
+    if (midVal < lastVal):
+        if ((target > midVal) and (target < lastVal)):
+            #in right side, seach in sorted array
+            return binarySearch(array, target, midIndex, rightIndex)
+        else:
+            return searchSortedIter(array, target, leftIndex, midIndex-1)
+    else:
+        if ((target < midVal) and (target > lastVal)):
+            #in left side, search in sorted array
+            return binarySearch(array, target, leftIndex, midIndex)
+        else:
+            return searchSortedIter(array, target, leftIndex+1, rightIndex)
+
+#helpful functions
+def binarySearch(array, target):
+    leftIndex = 0
+    rightIndex = len(array)-1
+    while (leftIndex <= rightIndex):
+        midIndex = int((leftIndex+rightIndex)/2)
+        midValue = array[midIndex]
+        if (midValue == target):
+            return midIndex
+        elif (target < midValue):
+            rightIndex = midIndex - 1
+        else:
+            leftIndex = midIndex + 1
+    return None
+
+#override
+def binarySearch(array, target, leftIndex, rightIndex):
+    while (leftIndex <= rightIndex):
+        midIndex = int((leftIndex+rightIndex)/2)
+        midValue = array[midIndex]
+        if (midValue == target):
+            return midIndex
+        elif (target < midValue):
+            rightIndex = midIndex - 1
+        else:
+            leftIndex = midIndex + 1
+    return None
+
 if __name__ == "__main__":
     print("Test Sorted Merge")
     arrayA = [1, 5, 7, 9, 10, 12]
@@ -62,3 +118,29 @@ if __name__ == "__main__":
     print("Test Group Anagrams")
     list = ["eat", "tea", "tan", "ate", "nat", "bat"]
     check(groupAnagrams(list), [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']])
+
+    print("---------------------------------")
+    print("Test Search in Sorted Array")
+    testArray = [15, 16, 19, 20, 25, 1 , 3, 4, 5, 7, 10, 14]
+    testArray2 = [15, 16, 19, 20, 25, 27 , 3, 4, 5, 7, 10, 14]
+    testArray3 = [1, 2, 5]
+    testArray4 = [1, 2]
+    testArray5 = [1]
+    check(searchSorted(testArray, 15), 0)
+    check(searchSorted(testArray, 16), 1)
+    check(searchSorted(testArray, 25), 4)
+    check(searchSorted(testArray, 1), 5)
+    check(searchSorted(testArray, 5), 8)
+    check(searchSorted(testArray, 14), 11)
+    check(searchSorted(testArray2, 15), 0)
+    check(searchSorted(testArray2, 16), 1)
+    check(searchSorted(testArray2, 25), 4)
+    check(searchSorted(testArray2, 3), 6)
+    check(searchSorted(testArray2, 5), 8)
+    check(searchSorted(testArray2, 14), 11)
+    check(searchSorted(testArray3, 1), 0)
+    check(searchSorted(testArray3, 2), 1)
+    check(searchSorted(testArray3, 5), 2)
+    check(searchSorted(testArray4, 1), 0)
+    check(searchSorted(testArray4, 2), 1)
+    check(searchSorted(testArray5, 1), 0)
